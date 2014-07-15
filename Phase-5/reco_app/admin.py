93208b8c220910fd1ba71739279b8e9eb491c0aa
Devanshu Jain <devanshu.jain919@gmail.com>
2014-07-15 17:36:13 +0530

    Recommendation system for adaptive e-learning

---
+++ Phase-5/reco_app/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from reco_app.models import *
+
+# Register your models here.
+
+admin.site.register(Student)
+admin.site.register(Course)
+admin.site.register(StudentState)
+admin.site.register(CourseDependency)
+admin.site.register(Question)
